b42e0271135bda76090a73d2305857ce2d9af613
AzkeGAS <goren_aguirre@hotmail.com>
2026-05-04 12:03:27 +0200

    Add Tradable.py for long position analysis
    Implement logic to determine tradable long positions based on runups and drawdowns.

---
+++ IBKR_AlgoTrading/Tradable.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+
+df_6M = pd.read_csv("DAX_Long_signals_Data.csv")
+runups = df_6M["runup"].dropna()
+drawdowns = df_6M["drawdown"].dropna()
+
+stop_points = df['ST_Long']
+
+P_take_hit = (runups > stop_points).mean()
+P_stop_hit = (drawdowns > stop_points).mean()
+
+if P_take_hit >= P_stop_hit
+  df['Tadrable'] = "True"
+  print("Long tradable:", stop_points)
